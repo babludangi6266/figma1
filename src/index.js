25f97678a87672acdc341ae484d9594e1d8d80e4
babludangi6266 <babludangi2000@gmail.com>
2024-04-15 15:12:51 +0530

    Second Updation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GrowthSchool from './component/Growth';
 import Page2 from './component/page2';
